c5c9c47bb81196f1d66992c7ca7eae26e301e82e
venkata sandeep kumar karamsetty <sandeepsandy2530@GMAIL.COM>
2020-04-17 00:20:48 +1000

    Add files via upload

---
+++ Data Structures lab/1a.cpp
@@ -0,0 +1,40 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Write a program to find the solution for the maximum subsequence sum problem using Linear Algorithm
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i, j, n, sum, a[20];
+	int alg4(int[], int);
+	n = 0;
+	std::cout << "\n\t Enter n value:";
+	std::cin >> n;
+	std::cout << "\n\t Enter array values:";
+	for (i = 0; i < n; i++) {
+		std::cout << "Enter array value" << n << ":";
+		std::cin >> a[i];
+	}
+
+	sum = alg4(a, n);
+	std::cout << "\n\t sum of linear algorithams is:" << sum;
+
+
+	return 0;
+}
+int alg4(int a[20], int n)
+{
+	int j, maxsum, thissum;
+	thissum = 0;
+	maxsum = 0;
+	for (j = 0; j < n; j++)
+	{
+		thissum += a[j];
+		if (thissum > maxsum)
+			maxsum = thissum;
+		else if (thissum < 0)
+			thissum = 0;
+	}
+	return maxsum;
+}
