2c8af47ec19b251d17bcfb2603ed25329dd53f6f
venkata sandeep kumar karamsetty <sandeepsandy2530@GMAIL.COM>
2020-04-17 01:12:57 +1000

    comments added

--- Data Structures lab/1a.cpp
+++ Data Structures lab/1a.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main()
 {
-	int i, j, n, sum, a[20];
-	int alg4(int[], int);
-	n = 0;
-	std::cout << "\n\t Enter n value:";
+	int i, j, n, sum, a[20];      // Data variable initialization
+	int alg4(int[], int);	      
+	n = 0;		              // Assigning
+	std::cout << "\n\t Enter n value:";	
 	std::cin >> n;
 	std::cout << "\n\t Enter array values:";
 	for (i = 0; i < n; i++) {
-		std::cout << "Enter array value" << n << ":";
-		std::cin >> a[i];
+		std::cout << "Enter array value" << i << ":";
+		std::cin >> a[i];                // Getting user array data
 	}
 
-	sum = alg4(a, n);
+	sum = alg4(a, n);      // calling function module and getting value
 	std::cout << "\n\t sum of linear algorithams is:" << sum;
 
 
@@ -30,11 +30,11 @@ int alg4(int a[20], int n)
 	maxsum = 0;
 	for (j = 0; j < n; j++)
 	{
-		thissum += a[j];
-		if (thissum > maxsum)
-			maxsum = thissum;
-		else if (thissum < 0)
-			thissum = 0;
+		thissum += a[j];	// summing next value	
+		if (thissum > maxsum)   // checking whether next is postive or not ?
+			maxsum = thissum;    // if +ve update maxsum
+		else if (thissum < 0)	// if its negative
+			thissum = 0;    // -ve leave the number update this number
 	}
-	return maxsum;
+	return maxsum;     
 }
